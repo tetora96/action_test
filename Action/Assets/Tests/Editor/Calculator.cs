2941275dc8595c2367ed9dd10fdbe20a6ef4b3c2
96 <tetora.96@gmail.com>
2023-06-19 01:59:02 +0900

    failed test

--- Action/Assets/Tests/Editor/Calculator.cs
+++ Action/Assets/Tests/Editor/Calculator.cs
@@ -9,7 +9,10 @@ public class Calculator
     [TestCase(1,2)]
     public void テスト(int a, int b){
         Add(a,b);
-
+    }
+    [TestCase(1)]
+    public void 失敗テスト(int a){
+        Assert.AreEqual(5, a);
     }
 
     public int Add(int a, int b){
